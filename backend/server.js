8d66dda28e1624729e221c9822c51df6143574b5
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2020-10-21 23:56:04 +0530

    fix err

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const data = require('./data');
 
-
 const app = express();
 app.use(express.json());
-app.use(cors);
-
+app.use(cors());
 
 app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
 
-    res.send(data.products);
-})
-
-app.listen(5000, () => {console.log("Server started at http://localhost:5000")})
+app.listen(5000, () => {
+  console.log('Server started at http://localhost:5000');
+});
